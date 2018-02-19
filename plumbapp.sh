fc6e639f2086d6c806a3605128192ae43e58aa95
kuzmenkov111 <kuzmenkov111@mail.ru>
2018-02-19 11:35:00 +0300

    Update plumbapp.sh

--- plumbapp.sh
+++ plumbapp.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
-then
-  echo "Using default demo app"
-  APP="/apps/demo.R"
-else
-  APP=$1
-fi
-
-su - plumber -c "R -e 'library(plumber); r <- plumb(\"${APP}\"); r\$run(port=8000)'"
+su - plumber -c "R -e 'library(plumber); r <- plumb("api.R"); r\$run(port=8000)'"
